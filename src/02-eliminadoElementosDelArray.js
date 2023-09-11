ad0fac958ba91cb57bb5ec1b96aeeb458719e665
abate2 <deibermoyano517@gmail.com>
2023-09-11 16:59:03 -0500

    Clase-10-hash tables

---
+++ src/02-eliminadoElementosDelArray.js
@@ -0,0 +1,41 @@
+//const array = ['Diego', 'Karen', 'Oscar'];
+
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  get(index) {
+    return this.data[index];
+  }
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.data;
+  }
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];//aqui estamos indicando entre [] el index que en esta caso la totalidadde los index sera igual a la longitud total del array -1 por que contamos desde 0
+    this.length--;
+    return lastItem;
+  }
+  delete(index) {
+    const item = this.data[index]
+    this.shiftIndex(index);
+
+    return item;
+  }
+  shiftIndex(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+  }
+    delete this.data[this.length -1];
+    this.legth --;
+}
+}
+
+const myArray = new MyArray();
+myArray.push("Diego");
+myArray.push("Karen");
+myArray.push("Oscar");
+myArray.delete(1);
